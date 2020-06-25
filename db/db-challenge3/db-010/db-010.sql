08c27d469df12f6af79d78b67fd5422a01c0d53d
RyomaY <docomoknps200uu@gmail.com>
2020-06-25 10:57:33 +0900

    Fix: 問題番号010のSQL文とキャプチャを修正

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -2,15 +2,16 @@ SELECT users.user_name AS ユーザー名,
   投稿日時,
   chats.chat_name AS チャットルーム名
 FROM (
-    SELECT chat_id,
-      MAX(created_at) AS 投稿日時
-    FROM posts
-    WHERE is_deleted = 0
-    GROUP BY chat_id
+    SELECT p.chat_id,
+      MAX(p.created_at) AS 投稿日時
+    FROM posts p
+      JOIN users u ON p.create_member_id = u.id
+    WHERE p.is_deleted = 0
+      AND u.is_deleted = 0
+    GROUP BY p.chat_id
   ) AS latest
   JOIN posts ON latest.chat_id = posts.chat_id
   AND latest.投稿日時 = posts.created_at
   JOIN chats ON posts.chat_id = chats.id
   JOIN users ON posts.create_member_id = users.id
-WHERE users.is_deleted = 0
 ORDER BY posts.chat_id;
