7990e5818625dcbe47c738c039942b0ccd8bf823
RyomaY <docomoknps200uu@gmail.com>
2020-06-25 17:24:58 +0900

    Fix: 問題番号012のSQL文とキャプチャを修正

--- db/db-challenge3/db-012/db-012.sql
+++ db/db-challenge3/db-012/db-012.sql
@@ -1,8 +1,9 @@
 BEGIN;
 UPDATE chats c
   JOIN chats_users u ON c.id = u.chat_id
-SET c.is_file_permission = 0
+SET c.is_file_permission = 0,
+  c.modified_at = CURRENT_TIMESTAMP,
+  c.modify_member_id = 1
 WHERE u.user_id <> 1
-  AND c.is_deleted = 0
-  AND c.modify_member_id = 1;
+  AND c.is_deleted = 0;
 COMMIT;
